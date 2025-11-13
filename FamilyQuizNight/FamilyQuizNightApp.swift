74e0e54f5c554ff218ab02296ce102be66dffbce
sachin <sachinharshitha179@gmail.com>
2025-11-13 18:32:46 +0530

    Initial Commit

---
+++ FamilyQuizNight/FamilyQuizNightApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamilyQuizNightApp.swift
+//  FamilyQuizNight
+//
+//  Created by yuvin on 2025-11-13.
+//
+
+import SwiftUI
+
+@main
+struct FamilyQuizNightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
